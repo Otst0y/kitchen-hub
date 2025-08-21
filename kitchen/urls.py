e1b7bf2de1ee29c8a5bd68af53379fa76331eb9f
Otstoy <otstoy.sxe@gmail.com>
2025-08-21 15:49:03 +0300

    refactor(templates): restructure templates and add sidebar include feat(auth): add CookCreationForm with registration template, view, and url

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from kitchen.views import HomeView
+from kitchen.views import HomeView, RegistrationView
 
 app_name = "kitchen"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("registration/", RegistrationView.as_view(), name="registration")
 ]
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views import generic
+
+from kitchen.forms import CookCreationForm
 
 
 class HomeView(LoginRequiredMixin, generic.TemplateView):
     template_name = "kitchen/home.html"
+
+
+class RegistrationView(generic.FormView):
+    template_name = "registration/registration.html"
+    form_class = CookCreationForm
+    success_url = reverse_lazy("kitchen:home")
