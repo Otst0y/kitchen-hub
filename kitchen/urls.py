d895ab487b0d341aee620c01ac0cc5f21694bd17
Otstoy <otstoy.sxe@gmail.com>
2025-08-21 16:38:32 +0300

    feat(dish): add base Dish model, DishType list/detail views, templates, and urls

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from kitchen.views import HomeView, RegistrationView
+from kitchen.views import (
+    HomeView,
+    RegistrationView,
+    DishTypeListView,
+    DishTypeDetailView,
+    DishDetailView,
+)
 
 app_name = "kitchen"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("registration/", RegistrationView.as_view(), name="registration")
+    path("registration/", RegistrationView.as_view(), name="registration"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dish-types/<int:pk>/", DishTypeDetailView.as_view(), name="dish-type-detail"),
+    path("dish/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
 ]
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Prefetch
 from django.urls import reverse_lazy
 from django.views import generic
 
 from kitchen.forms import CookCreationForm
+from kitchen.models import DishType, Dish
 
 
 class HomeView(LoginRequiredMixin, generic.TemplateView):
@@ -13,3 +15,24 @@ class RegistrationView(generic.FormView):
     template_name = "registration/registration.html"
     form_class = CookCreationForm
     success_url = reverse_lazy("kitchen:home")
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    template_name = "kitchen/dish_types.html"
+
+
+class DishTypeDetailView(generic.DetailView):
+    model = DishType
+    template_name = "kitchen/dish_type_detail.html"
+    queryset = DishType.objects.prefetch_related(
+        Prefetch(
+            "dishes",
+            queryset=Dish.objects.prefetch_related("cooks")
+        )
+    )
+
+
+class DishDetailView(generic.DetailView):
+    model = Dish
+    template_name = "kitchen/dish_detail.html"
