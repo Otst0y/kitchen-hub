bfb01a463905ebd36729bbb2a964b6de7a2c1fc1
Otstoy <otstoy.sxe@gmail.com>
2025-08-21 12:02:49 +0300

    Add verbose_name_plural for Dish model and customize field display in admin.

--- kitchen/models.py
+++ kitchen/models.py
@@ -44,6 +44,7 @@ class Dish(models.Model):
 
     class Meta:
         ordering = ["name"]
+        verbose_name_plural = "Dishes"
 
     def __str__(self):
         return f"{self.name} ${self.price}"
